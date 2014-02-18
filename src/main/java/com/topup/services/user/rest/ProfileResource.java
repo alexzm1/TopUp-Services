7017190307f9a4069838a838b2fee7404fe06572
alexzm1 <alexzm1@gmail.com>
2014-02-18 00:16:18 -0500

    Working with spring security and MOXy to convert Java Objects into JSON

--- src/main/java/com/topup/services/user/rest/ProfileResource.java
+++ src/main/java/com/topup/services/user/rest/ProfileResource.java
@@ -9,31 +9,41 @@ import com.topup.services.user.model.UserProfile;
 import com.topup.services.user.service.UserProfileService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Samuel Alejandro
  */
-@Component
-@Path("/user")
+@Repository
+@Path("/api")
 public class ProfileResource {
 
     @Autowired
     private UserProfileService userProfileService;
 
     @GET
+    @PreAuthorize("hasRole('ROLE_DUMMY')")
+    @Path("secure/user")
+    @Produces(MediaType.APPLICATION_JSON)
     public UserProfile getProfile() {
         return userProfileService.getUserProfile();
     }
-    
+
     @GET
-    @Path("/test")
-    //@Produces("appli")
-    public UserProfile testProfile(){
-        return new UserProfile();
+    @Path("user/{mobile_number}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public UserProfile getMobileNumberIdStatus(@PathParam("mobile_number") String mobileNumber) {
+        UserProfile profile = new UserProfile();
+        profile.setId("87362786");
+        profile.setPhoneNumber("287765654");
+        return profile;
     }
 
 }
