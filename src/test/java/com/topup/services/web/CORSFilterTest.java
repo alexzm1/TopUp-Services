39cc6b337252c461373ca2c4dd153c523977517c
alexzm1 <alexzm1@gmail.com>
2014-05-28 22:45:58 -0400

    Added JUnit test for CORSFilter

---
+++ src/test/java/com/topup/services/web/CORSFilterTest.java
@@ -0,0 +1,44 @@
+package com.topup.services.web;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class CORSFilterTest {
+
+	private MockFilterChain mockFilterChain;
+	private MockHttpServletRequest httpServletRequest;
+	private MockHttpServletResponse httpServletResponse;
+
+	@Before
+	public void setup() {
+		mockFilterChain = new MockFilterChain();
+		httpServletRequest = new MockHttpServletRequest("GET",
+				"/api/telephone/1234567890");
+		httpServletResponse = new MockHttpServletResponse();
+	}
+
+	@Test
+	public void testDoFilte() throws IOException, ServletException {
+		final CORSFilter corsFilter = new CORSFilter();
+		corsFilter.doFilter(httpServletRequest, httpServletResponse,
+				mockFilterChain);
+		assertEquals("POST, GET, OPTIONS, DELETE",
+				httpServletResponse.getHeader("Access-Control-Allow-Methods"));
+		assertEquals("*",
+				httpServletResponse.getHeader("Access-Control-Allow-Origin"));
+		assertEquals("3600",
+				httpServletResponse.getHeader("Access-Control-Max-Age"));
+		assertEquals("x-requested-with",
+				httpServletResponse.getHeader("Access-Control-Allow-Headers"));
+	}
+
+}
